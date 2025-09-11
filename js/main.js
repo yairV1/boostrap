6048599f0d9e4f35647b10a91825c9ad0b789290
Yair <yandrey2007@gmail.com>
2025-09-11 16:04:17 -0500

    Finalizamos ek textimonials

--- js/main.js
+++ js/main.js
@@ -31,3 +31,37 @@ $(document).ready(function(){
         ]
     });
 });
+
+$(document).ready(function(){
+    $('.slider-reviews').slick({
+    dots: false,
+    infinite: true,
+    arrows:true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+        responsive:[
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                },
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+                },
+                {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            
+        ]
+    });
+});
